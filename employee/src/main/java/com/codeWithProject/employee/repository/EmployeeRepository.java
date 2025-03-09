bd91dd7ba02143b35fd253d4c5e7d0f524d27948
Shayanthavi <shayanthavi_t_e23@engug.ruh.ac.lk>
2025-03-09 16:26:07 +0530

    backend

--- employee/src/main/java/com/codeWithProject/employee/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codeWithProject.employee.repository;
-
-import com.codeWithProject.employee.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-   }
